0dc67da6746442db4280eec03ff412ca7c760916
pskyhx <88959296+pskyhx@users.noreply.github.com>
2022-02-20 01:40:05 -0500

    fix typo in script

--- makegallery.sh
+++ makegallery.sh
@@ -5,7 +5,7 @@ lazygal \
 	--output-directory="docs" \
 	--force-gen-pages \
 	--clean-destination \
-	--image-size="large=3000x3000"
+	--image-size="large=3000x3000" \
 	--image-size="medium=1000x1000" \
 	--image-size="small=600x600" \
 	--thumbnail-size="200x200" \
