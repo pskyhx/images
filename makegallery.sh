b316ab0a4e5f08461eefacc7dfb679c799af8bc7
pskyhx <88959296+pskyhx@users.noreply.github.com>
2022-02-20 01:39:35 -0500

    add album metadata

---
+++ makegallery.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+lazygal \
+	--output-directory="docs" \
+	--force-gen-pages \
+	--clean-destination \
+	--image-size="large=3000x3000"
+	--image-size="medium=1000x1000" \
+	--image-size="small=600x600" \
+	--thumbnail-size="200x200" \
+	--original \
+	--puburl="feed" \
+	--thumbs-per-page="0" \
+	--keep-gps-data \
+	--no-video \
+	images
+
+
